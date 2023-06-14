65185fb12aca61e512a496101fbcfa7f353cbbb6
Chrystalio <chrystalio.k@outlook.com>
2023-06-14 17:48:17 +0700

    Change image

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -70,7 +70,7 @@
         <div class="row justify-content-center">
             <div class="col-md-12 col-lg-10">
                 <div class="wrap d-md-flex">
-                    <div class="img" style="background-image: url('https://source.unsplash.com/random/?company');">
+                    <div class="img" style="background-image: url('https://source.unsplash.com/random/?office');">
                     </div>
                     <div class="login-wrap p-4 p-md-5">
                         <div class="d-flex">
