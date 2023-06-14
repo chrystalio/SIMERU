776169c8d223ba8aa3aca5ae4dc972c5d7512229
Chrystalio <chrystalio.k@outlook.com>
2023-06-14 18:17:01 +0700

    Add datatable

--- resources/views/includes/script.blade.php
+++ resources/views/includes/script.blade.php
@@ -8,8 +8,13 @@
 <script src="{{ asset('assets/js/stisla.js') }}"></script>
 
 <!-- JS Libraies -->
+<script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.13.4/js/dataTables.bootstrap4.min.js"></script>
 
 <!-- Page Specific JS File -->
+<script>
+    let karyawanTable = new DataTable('#myTable')
+</script>
 
 <!-- Template JS File -->
 <script src="{{ asset('assets/js/scripts.js') }}"></script>
--- resources/views/includes/style.blade.php
+++ resources/views/includes/style.blade.php
@@ -1,6 +1,7 @@
 <!-- General CSS Files -->
 <link rel="stylesheet" href="{{ asset('assets/modules/bootstrap/css/bootstrap.min.css') }}">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+<link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css">
 
 <!-- CSS Libraries -->
 
