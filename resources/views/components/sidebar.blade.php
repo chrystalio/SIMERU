c1188059cc1362de5f9a1005b4ffddbfc0f72db5
Chrystalio <chrystalio.k@outlook.com>
2023-06-15 06:12:53 +0700

    Set active link by route

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -7,9 +7,9 @@
             <a href="index.html">SP</a>
         </div>
         <ul class="sidebar-menu">
-            <li class=active><a class="nav-link" href="{{ url('/dashboard') }}"><i class="fa-solid fa-layer-group"></i> <span>Dashboard</span></a></li>
-            <li class=""><a class="nav-link" href="{{ url('/dashboard/karyawan') }}"><i class="fa-solid fa-users"></i> <span>Karyawan</span></a></li>
-            <li class=""><a class="nav-link" href="{{ url('/dashboard/department') }}"><i class="fa-solid fa-building"></i> <span>Department</span></a></li>
+            <li class={{ request()->routeIs('dashboard') ? 'active' : ' ' }}><a class="nav-link" href="{{ url('/dashboard') }}"><i class="fa-solid fa-layer-group"></i> <span>Dashboard</span></a></li>
+            <li class="{{ request()->routeIs('karyawan.index') ? 'active' : ' ' }}"><a class="nav-link" href="{{ url('/dashboard/karyawan') }}"><i class="fa-solid fa-users"></i> <span>Karyawan</span></a></li>
+            <li class="{{ request()->routeIs('department.index') ? 'active' : ' ' }}"><a class="nav-link" href="{{ url('/dashboard/department') }}"><i class="fa-solid fa-building"></i> <span>Department</span></a></li>
         </ul>
     </aside>
 </div>
