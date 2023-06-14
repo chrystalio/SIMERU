e65cbfec39ac8b0e5991fc1b2a25b76b9807160a
Chrystalio <chrystalio.k@outlook.com>
2023-06-14 17:45:51 +0700

    Change heading-section

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -64,7 +64,7 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-6 text-center mb-5">
-                <h2 class="heading-section">SIMP</h2>
+                <h2 class="heading-section">SISTEM INFORMASI MANAGEMENT PERUSAHAAN</h2>
             </div>
         </div>
         <div class="row justify-content-center">
