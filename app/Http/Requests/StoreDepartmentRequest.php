dcce5f8341448014eed60f1503c042b90c34b606
Chrystalio <chrystalio.k@outlook.com>
2023-06-15 05:44:26 +0700

    crud_department add Request validation

---
+++ app/Http/Requests/StoreDepartmentRequest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateDepartmentRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|max:255|unique:departments,name,' . $this->department->id,
+        ];
+    }
+}
+
