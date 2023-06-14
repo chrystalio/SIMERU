b32d24345dd877a6fe9ef830793e0ff38a0f447d
Chrystalio <chrystalio.k@outlook.com>
2023-06-14 15:26:59 +0700

    fe-integration - implement templating blade add script and style

---
+++ resources/views/includes/style.blade.php
@@ -0,0 +1,9 @@
+<!-- General CSS Files -->
+<link rel="stylesheet" href="{{ asset('assets/modules/bootstrap/css/bootstrap.min.css') }}">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+
+<!-- CSS Libraries -->
+
+<!-- Template CSS -->
+<link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+<link rel="stylesheet" href="{{ asset('assets/css/components.css') }}">
