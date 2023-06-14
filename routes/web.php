d951dd94b8a586dc83633ed3a118730e2ea32152
Chrystalio <chrystalio.k@outlook.com>
2023-06-14 17:45:38 +0700

    Refactor web.php

--- routes/web.php
+++ routes/web.php
@@ -31,11 +31,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
 });
-
-Route::get('/logout', [AuthenticatedSessionController::class, 'destroy'])
-                ->middleware('auth')
-                ->name('logout');
-
 
 require __DIR__.'/auth.php';
