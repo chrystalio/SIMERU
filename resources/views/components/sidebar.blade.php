537501e97c5fda1b61b753ecfc87f8bc1a9717f3
Chrystalio <chrystalio.k@outlook.com>
2023-06-21 13:05:03 +0700

    Add Department sidebar menu

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -9,6 +9,7 @@
         <ul class="sidebar-menu">
             <li class={{ request()->routeIs('dashboard') ? 'active' : ' ' }}><a class="nav-link" href="{{ url('/dashboard') }}"><i class="fa-solid fa-layer-group"></i> <span>Dashboard</span></a></li>
             <li class="{{ request()->routeIs('karyawan.index') ? 'active' : ' ' }}"><a class="nav-link" href="{{ url('/dashboard/karyawan') }}"><i class="fa-solid fa-users"></i> <span>Karyawan</span></a></li>
+            <li class="{{ request()->routeIs('karyawan.index') ? 'active' : ' ' }}"><a class="nav-link" href="{{ url('/dashboard/department') }}"><i class="fa-solid fa-building"></i> <span>Department</span></a></li>
             <li class="{{ request()->routeIs('proyek.index') ? 'active' : ' ' }}"><a class="nav-link" href="{{ url('/dashboard/proyek') }}"><i class="fa-solid fa-folder"></i> <span>Projects</span></a></li>
             <li class="{{ request()->routeIs('laporan.index') ? 'active' : ' ' }}"><a class="nav-link" href="{{ url('/dashboard/laporan') }}"><i class="fa-solid fa-paper-plane"></i> <span>Laporan</span></a></li>
             <li class="{{ request()->routeIs('klien.index') ? 'active' : ' ' }}"><a class="nav-link" href="{{ url('/dashboard/klien') }}"><i class="fa-solid fa-circle-user"></i> <span>Klien</span></a></li>
