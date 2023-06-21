485492641411a979045e74a6957638620a025362
Chrystalio <chrystalio.k@outlook.com>
2023-06-21 14:28:54 +0700

    Change title dashboard

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
-    <title>Blank Page &mdash; Stisla</title>
+    <title>Dashboard &mdash; SIMERU</title>
     @include('includes.style')
 </head>
 
