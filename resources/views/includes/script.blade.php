cf307b3149dee900ef10a68d9146c267fb83360e
Chrystalio <chrystalio.k@outlook.com>
2023-07-01 17:50:45 +0700

    Refactor script blade files

--- resources/views/includes/script.blade.php
+++ resources/views/includes/script.blade.php
@@ -4,15 +4,12 @@
 <script src="{{ asset('assets/modules/tooltip.js') }}"></script>
 <script src="{{ asset('assets/modules/bootstrap/js/bootstrap.min.js') }}"></script>
 <script src="{{ asset('assets/modules/nicescroll/jquery.nicescroll.min.js') }}"></script>
-{{--<script src="{{ asset('assets/modules/moment.min.js') }}"></script>--}}
 <script src="{{ asset('assets/js/stisla.js') }}"></script>
 
 <!-- JS Libraies -->
 <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
 <script src="https://cdn.datatables.net/1.13.4/js/dataTables.bootstrap4.min.js"></script>
-<script src="https://code.highcharts.com/highcharts.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-<script src="https://code.highcharts.com/modules/variable-pie.js"></script>
 
 <!-- Page Specific JS File -->
 <script>
@@ -21,182 +18,6 @@
     let projectTable = new DataTable('#projectTable')
     let laporanTable = new DataTable('#laporanTable')
     let klienTable = new DataTable('#klienTable')
-
-    document.addEventListener('DOMContentLoaded', function() {
-        const chart = Highcharts.chart('container', {
-            chart: {
-                type: 'column'
-            },
-            title: {
-                text: 'Project Percentage'
-            },
-            xAxis: {
-                categories: [
-                    'Jan',
-                    'Feb',
-                    'Mar',
-                    'Apr',
-                    'May',
-                    'Jun',
-                    'Jul',
-                    'Aug',
-                    'Sep',
-                    'Oct',
-                    'Nov',
-                    'Dec'
-                ],
-                crosshair: true
-            },
-            yAxis: {
-                min: 0,
-                title: {
-                    text: 'Progress (%)'
-                }
-            },
-            tooltip: {
-                headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
-                pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
-                    '<td style="padding:0"><b>{point.y:.1f}%</b></td></tr>',
-                footerFormat: '</table>',
-                shared: true,
-                useHTML: true
-            },
-            plotOptions: {
-                column: {
-                    pointPadding: 0.2,
-                    borderWidth: 0
-                }
-            },
-            series: [{
-                name: 'Pemerintah',
-                data: [50, 70, 90, 80, 60, 75, 85, 95, 100, 90, 80, 70]
-            }, {
-                name: 'Swasta',
-                data: [70, 60, 85, 95, 80, 90, 75, 60, 70, 85, 90, 80]
-            }, {
-                name: 'Lainnya',
-                data: [80, 90, 75, 70, 85, 60, 80, 90, 70, 95, 75, 85]
-            }]
-        });
-
-        const chart1 = Highcharts.chart('container1', {
-            chart: {
-                plotBackgroundColor: null,
-                plotBorderWidth: null,
-                plotShadow: false,
-                type: 'pie'
-            },
-            title: {
-                text: 'Employee Percentage',
-                align: 'center'
-            },
-            tooltip: {
-                pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
-            },
-            accessibility: {
-                point: {
-                    valueSuffix: '%'
-                }
-            },
-            plotOptions: {
-                pie: {
-                    allowPointSelect: true,
-                    cursor: 'pointer',
-                    dataLabels: {
-                        enabled: false
-                    },
-                    showInLegend: true
-                }
-            },
-            series: [{
-                name: 'Total',
-                colorByPoint: true,
-                data: [{
-                    name: 'Managers',
-                    y: 25,
-                }, {
-                    name: 'Programmers',
-                    y: 35,
-                    sliced: true,
-                    selected: true
-                }, {
-                    name: 'Designers',
-                    y: 15
-                }, {
-                    name: 'HRD',
-                    y: 10
-                }, {
-                    name: 'Sales Representatives',
-                    y: 10
-                }, {
-                    name: 'Other',
-                    y: 5
-                }]
-            }]
-        });
-
-        const chart3 = Highcharts.chart('projectStatus', {
-                chart: {
-                    plotBackgroundColor: null,
-                    plotBorderWidth: null,
-                    plotShadow: true,
-                    type: 'pie'
-                },
-                title: {
-                    text: 'Project Status Percentage',
-                    align: 'center'
-                },
-                tooltip: {
-                    pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
-                },
-                accessibility: {
-                    point: {
-                        valueSuffix: '%'
-                    }
-                },
-                plotOptions: {
-                    pie: {
-                        allowPointSelect: true,
-                        cursor: 'pointer',
-                        dataLabels: {
-                            enabled: true,
-                            format: '<b>{point.name}</b>: {point.percentage:.1f} %'
-                        }
-                    }
-                },
-                series: [{
-                    name: 'Progress',
-                    colorByPoint: true,
-                    data: [{
-                        name: 'NOT STARTED',
-                        y: 55.67,
-                    }, {
-                        name: 'ON PROGRESS',
-                        y: 14.77
-                    },  {
-                        name: 'PENDING',
-                        y: 42.86
-                    }, {
-                        name: 'FINISHED',
-                        y: 90.53,
-                        sliced: true,
-                        selected: true
-                    }, {
-                        name: 'CANCELLED',
-                        y: 23.63
-                    },]
-                }],
-                colors: [
-                    '#191d21',
-                    '#6777ef',
-                    '#ff9900',
-                    '#47c363',
-                    '#fc544b'
-                ]
-            });
-
-    });
-
 </script>
 
 <!-- Template JS File -->
