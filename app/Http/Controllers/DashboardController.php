29ad4e0cc3f68b9b6d6dfed876158b2c72239f83
Chrystalio <chrystalio.k@outlook.com>
2023-07-01 14:36:05 +0700

    Implement single action invoke DashboardController

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,15 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Karyawan;
 use App\Models\Proyek;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 
 class DashboardController extends Controller
 {
-    public function index(): View
+    public function __invoke()
     {
-        $proyekData = Proyek::query()->with(['karyawan'])->latest('updated_at')->take(5)->get();
-        return view('dashboard', compact('proyekData'));
+        $proyekCount = Proyek::count();
+        $karyawanCount = Karyawan::count();
+        $proyekData = Proyek::with('karyawan')->latest('updated_at')->take(5)->get();
+
+
+        return view('dashboard', compact('proyekCount', 'karyawanCount', 'proyekData'));
     }
 }
+
