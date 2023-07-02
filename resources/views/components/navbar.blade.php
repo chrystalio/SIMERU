ad7f0c5a4629ee0f49767d928ec1be10c6f8d675
Chrystalio <chrystalio.k@outlook.com>
2023-07-02 17:17:10 +0700

    Remove Setting menu

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -12,9 +12,6 @@
                 <a href="features-profile.html" class="dropdown-item has-icon">
                     <i class="far fa-user"></i> Profile
                 </a>
-                <a href="features-settings.html" class="dropdown-item has-icon">
-                    <i class="fas fa-cog"></i> Settings
-                </a>
                 <div class="dropdown-divider"></div>
                 <form action="{{ route('logout') }}" id="form-logout" method="POST">
                     @csrf
