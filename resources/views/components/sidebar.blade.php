7fd11e6d91e11e4b89945871c103b117885e0168
Chrystalio <chrystalio.k@outlook.com>
2023-08-20 02:30:38 +0700

    Add user menu on sidebar for admin

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -13,6 +13,9 @@
             <li class="{{ request()->routeIs('proyek.index') ? 'active' : ' ' }}"><a class="nav-link" href="{{ url('/dashboard/proyek') }}"><i class="fa-solid fa-folder"></i> <span>Projects</span></a></li>
             <li class="{{ request()->routeIs('laporan.index') ? 'active' : ' ' }}"><a class="nav-link" href="{{ url('/dashboard/laporan') }}"><i class="fa-solid fa-paper-plane"></i> <span>Laporan</span></a></li>
             <li class="{{ request()->routeIs('klien.index') ? 'active' : ' ' }}"><a class="nav-link" href="{{ url('/dashboard/klien') }}"><i class="fa-solid fa-circle-user"></i> <span>Klien</span></a></li>
+           @if(auth()->user()->role()->first()->name === 'System Administrator')
+                <li class="{{ request()->routeIs('user.index') ? 'active' : ' ' }}"><a class="nav-link" href="{{ url('/dashboard/user') }}"><i class="fa-solid fa-users"></i> <span>Users</span></a></li
+           @endif
         </ul>
     </aside>
 </div>
