bfe2cbb6ee80649d2aa1d0f60bf73103e08389df
Chrystalio <chrystalio.k@outlook.com>
2023-08-20 00:37:15 +0700

    Add role_id column in user table migration

---
+++ database/migrations/2023_08_19_231342_add_role_id_column_in_user_table.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->unsignedBigInteger('role_id')->after('email')->default(4);
+            $table->foreign('role_id')->references('id')->on('roles');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropForeign(['role_id']);
+            $table->dropColumn(['role_id']);
+        });
+    }
+};
