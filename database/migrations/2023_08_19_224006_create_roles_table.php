dcd2a6aa672c180a722b935c86b3d159b5e5c0df
Chrystalio <chrystalio.k@outlook.com>
2023-08-20 01:05:28 +0700

    Set name to be unique

--- database/migrations/2023_08_19_224006_create_roles_table.php
+++ database/migrations/2023_08_19_224006_create_roles_table.php
@@ -9,7 +9,7 @@ return new class extends Migration {
     {
         Schema::create('roles', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->unique();
             $table->timestamps();
         });
     }
