acb8e02772767e8ac1e11bb7631a0e52f9869040
Chrystalio <chrystalio.k@outlook.com>
2023-08-20 02:30:03 +0700

    Add User view blade

---
+++ resources/views/admin/users/index.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.app')
+@section('title')
+    Dashboard
+@endsection
+@section('content')
+    <section class="section">
+        <div class="section-header">
+            <h1>Users</h1>
+        </div>
+        <div class="section-body">
+            <div class="card-body bg-white my-2 rounded">
+                <div class="">
+                    <table class="table table-md table-hover table-bordered" id="departmentTable">
+                        <thead>
+                        <tr>
+                            <th>No</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Role</th>
+                            <th>Action</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @foreach($users as $user)
+                            <tr>
+                                <td>{{ $loop->iteration }}</td>
+                                <td>{{ $user->name }}</td>
+                                <td>{{ $user->email }}</td>
+                                <td class="text-capitalize">{{ $user->role()->first()->name }}</td>
+                                <td>
+                                    <a href="#" class="btn btn-danger">
+                                        <i class="fa fa-key"></i>
+                                    </a>
+                                </td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
