1ec915667d5d7f3a9c93bfe5d56745d7456d2e71
Chrystalio <chrystalio.k@outlook.com>
2023-08-19 23:11:34 +0700

    Add Role Model

---
+++ app/Models/Role.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Role extends Model
+{
+    protected $primaryKey = 'id';
+    protected $table = 'roles';
+
+    protected $guarded = ['id', 'created_at', 'updated_at'];
+
+    protected $fillable = [
+        'name',
+    ];
+
+    public function users(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class);
+    }
+}
