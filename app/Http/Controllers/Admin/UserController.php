7cf1432a79fe57038d4bd6bf64b50ae28e6d7807
Chrystalio <chrystalio.k@outlook.com>
2023-08-20 16:58:56 +0700

    Set return type on forceResetPassword method

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use Illuminate\Http\RedirectResponse;
 
 class UserController extends Controller
 {
@@ -14,7 +15,7 @@ class UserController extends Controller
         return view('admin.users.index', compact('users'));
     }
 
-    public function forceResetPassword($id)
+    public function forceResetPassword($id): RedirectResponse
     {
         $user = User::find($id);
 
